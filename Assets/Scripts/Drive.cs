f2554d975ff8ea44b1e6bda7d4b5238582f38146
GaozyDev <670453367@qq.com>
2021-12-31 23:29:17 +0800

    init

---
+++ Assets/Scripts/Drive.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    public List<GameObject> wheels;
+
+    void Update()
+    {
+        transform.position += new Vector3(0, 0, 8f * Time.deltaTime);
+
+        foreach (var wheel in wheels)
+        {
+            wheel.transform.Rotate(300f * Time.deltaTime, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,45 @@
+﻿//键盘控制using UnityEngine;
+
+using System.Collections;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+    // Use this for initialization
+    private GameObject gameObject;
+    float x1;
+    float x2;
+    float x3;
+    float x4;
+
+    void Start() {
+        gameObject = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //空格键抬升高度
+        if (Input.GetKey(KeyCode.Space)) {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+        }
+
+        //w键前进
+        if (Input.GetKey(KeyCode.W)) {
+            this.gameObject.transform.Translate(new Vector3(0, 0, 5 * Time.deltaTime));
+        }
+
+        //s键后退
+        if (Input.GetKey(KeyCode.S)) {
+            this.gameObject.transform.Translate(new Vector3(0, 0, -5 * Time.deltaTime));
+        }
+
+        //a键后退
+        if (Input.GetKey(KeyCode.A)) {
+            this.gameObject.transform.Translate(new Vector3(-5 * Time.deltaTime, 0, 0));
+        }
+
+        //d键后退
+        if (Input.GetKey(KeyCode.D)) {
+            this.gameObject.transform.Translate(new Vector3(5 * Time.deltaTime, 0, 0));
+        }
+    }
+}
