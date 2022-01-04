48f963d6c1df02cc81639fe45edfbdf84b3c4758
gaozy <zongyang.gao@xiaoxitech.com>
2022-01-04 17:27:53 +0800

    相机围绕汽车旋转

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -1,45 +1,12 @@
-﻿//键盘控制using UnityEngine;
-
-using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MainCamera : MonoBehaviour {
-    // Use this for initialization
-    private GameObject gameObject;
-    float x1;
-    float x2;
-    float x3;
-    float x4;
-
-    void Start() {
-        gameObject = GameObject.Find("Main Camera");
-    }
+    
+    public GameObject car;
 
     // Update is called once per frame
     void Update() {
-        //空格键抬升高度
-        if (Input.GetKey(KeyCode.Space)) {
-            transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
-        }
-
-        //w键前进
-        if (Input.GetKey(KeyCode.W)) {
-            this.gameObject.transform.Translate(new Vector3(0, 0, 5 * Time.deltaTime));
-        }
-
-        //s键后退
-        if (Input.GetKey(KeyCode.S)) {
-            this.gameObject.transform.Translate(new Vector3(0, 0, -5 * Time.deltaTime));
-        }
-
-        //a键后退
-        if (Input.GetKey(KeyCode.A)) {
-            this.gameObject.transform.Translate(new Vector3(-5 * Time.deltaTime, 0, 0));
-        }
-
-        //d键后退
-        if (Input.GetKey(KeyCode.D)) {
-            this.gameObject.transform.Translate(new Vector3(5 * Time.deltaTime, 0, 0));
-        }
+        transform.position += new Vector3(0, 0, 16f * Time.deltaTime);
+        transform.RotateAround(car.transform.position, new Vector3(0, 1, 0), 20f * Time.deltaTime);
     }
 }
