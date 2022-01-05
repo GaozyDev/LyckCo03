1b015aea641221314c22d3c348f1a0a73aacc7e0
gaozy <zongyang.gao@xiaoxitech.com>
2022-01-05 15:05:22 +0800

    优化动画

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -2,35 +2,43 @@
 using DG.Tweening;
 using UnityEngine;
 
-public class MainCamera : MonoBehaviour
-{
+public class MainCamera : MonoBehaviour {
     public GameObject car;
 
     private bool _isZoomIn = true;
 
+    private bool _rise = true;
+
     private float _time = 0;
-    
-    private void Start()
-    {
-        transform.DOMoveY(1.33f, 2).SetEase(Ease.Linear);
-        
-        transform.DOMoveZ(92, 5).SetEase(Ease.Linear).onComplete += () =>
-        {
-            _isZoomIn = false; 
-            transform.DOMoveY(3.1f, 3).SetEase(Ease.Linear).onComplete += () =>
-            {
-                
-            };
-        };
+
+    private void Start() {
+        // 摄像机抬头
+        transform.DORotate(new Vector3(8, 0, 0), 3);
     }
 
-    void Update()
-    {
+    void Update() {
         _time += Time.deltaTime;
-        if (!_isZoomIn)
-        {
+
+        if (_isZoomIn) {
+            // 摄像机拉近
+            transform.position += new Vector3(0, 0, 18f * Time.deltaTime);
+            
+            if (car.transform.position.z - transform.position.z < 16) {
+                transform.RotateAround(car.transform.position, new Vector3(0, 1, 0), -3.5f * Time.deltaTime);
+            }
+
+            if (car.transform.position.z - transform.position.z < 9) {
+                _isZoomIn = false;
+            }
+        } else {
             transform.position += new Vector3(0, 0, 16f * Time.deltaTime);
-            transform.RotateAround(car.transform.position, new Vector3(0, 1, 0), -20f * Time.deltaTime);
+            transform.RotateAround(car.transform.position, new Vector3(0, 1, 0), -4.5f * Time.deltaTime);
+        }
+
+        if (_rise && transform.rotation.y < -0.99f) {
+            Debug.Log("DOMoveY");
+            transform.DOMoveY(3f, 15).SetEase(Ease.Linear);
+            _rise = false;
         }
     }
 }
